a9fb4e24cde4104c67ef7980d4c44aea70e5089d
imsidkg <siddharth8271skg2@gmail.com>
2024-09-04 03:09:54 +0530

    shadcn updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen  flex-col items-center justify-between p-24">
-        <Button/>
+        <Button>hifffffff</Button>
+        <Input/>
     </main>
   );
 }
